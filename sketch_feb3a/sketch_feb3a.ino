6f06e867237af2525f33ffb987373f2d9c5e594c
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2025-02-09 18:22:52 +0200

    ESP32 Remote-Controlled Wi-Fi Car Robot

--- sketch_feb3a/sketch_feb3a.ino
+++ sketch_feb3a/sketch_feb3a.ino
@@ -1,10 +1,3 @@
-/*  
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  https://RandomNerdTutorials.com/esp32-wi-fi-car-robot-arduino/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 #include <WiFi.h>
 #include <WebServer.h>
 
